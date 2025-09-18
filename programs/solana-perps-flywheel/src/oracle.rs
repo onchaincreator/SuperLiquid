dd6347292f8c8d3db0f4531abf282d5fd9946029
onchaincreator <pokemeplease2@outlook.com>
2025-09-18 14:25:14 +0800

    Create oracle.rs

---
+++ programs/solana-perps-flywheel/src/oracle.rs
@@ -0,0 +1,10 @@
+use anchor_lang::prelude::*;
+use crate::errors::PerpsError;
+use crate::state::{OraclePrice};
+
+
+pub fn read_oracle_fp(oracle: &Account<OraclePrice>) -> Result<u128> {
+let now = Clock::get()?.unix_timestamp;
+require!(now - oracle.last_updated_ts < 120, PerpsError::BadOracle);
+Ok(oracle.price_fp)
+}
