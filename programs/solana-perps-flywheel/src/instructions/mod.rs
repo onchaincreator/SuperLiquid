7fb58a66f389d1713d96183628dac5b4360cdbd5
onchaincreator <pokemeplease2@outlook.com>
2025-09-18 14:25:38 +0800

    Create mod.rs

---
+++ programs/solana-perps-flywheel/src/instructions/mod.rs
@@ -0,0 +1,14 @@
+pub mod admin;
+pub mod create_market;
+pub mod trade;
+pub mod liquidate;
+pub mod funding;
+pub mod rewards;
+
+
+pub use admin::*;
+pub use create_market::*;
+pub use trade::*;
+pub use liquidate::*;
+pub use funding::*;
+pub use rewards::*;
